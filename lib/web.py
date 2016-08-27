889c726ce96de2abddc48ccebaa13b1a2efd308b
Jakub Wilk <jwilk@jwilk.net>
2016-08-27 16:52:33 +0200

    lib.web: follow redirects.

--- lib/web.py
+++ lib/web.py
@@ -25,12 +25,14 @@ HTTP client
 import http
 import logging
 import ssl
+import urllib.parse
 
 import aiohttp
 import werkzeug.urls
 
 user_agent = 'urlycue (https://github.com/jwilk/urlycue)'
 http_headers = {'User-Agent': user_agent}
+redirect_limit = 10
 
 class Status(object):
     '''
@@ -40,6 +42,16 @@ class Status(object):
     def __init__(self, *, code, location=None):
         self.code = http.HTTPStatus(code)  # pylint: disable=no-value-for-parameter
         self.location = location
+        self.temporary_redirect = code in {
+            http.HTTPStatus.FOUND,
+            http.HTTPStatus.SEE_OTHER,
+            http.HTTPStatus.TEMPORARY_REDIRECT,
+        }
+        self.permanent_redirect = code in {
+            http.HTTPStatus.MOVED_PERMANENTLY,
+            http.HTTPStatus.PERMANENT_REDIRECT,
+        }
+        self.redirect = self.temporary_redirect or self.permanent_redirect
         self.ok = self.code == http.HTTPStatus.OK
 
     def __str__(self):
@@ -49,14 +61,37 @@ status_ok = Status(code=http.HTTPStatus.OK)
 assert status_ok.ok
 
 async def _check_url(session, url):
-    async with session.head(url, allow_redirects=False) as response:
-        try:
-            status = Status(
-                code=response.status,
-                location=response.headers.get('Location')
-            )
-        except ValueError as exc:
-            status = exc
+    logger = logging.getLogger('urlycue')
+    n_redirects = 0
+    redirect_status = True
+    while True:
+        async with session.head(url, allow_redirects=False) as response:
+            location = response.headers.get('Location')
+            if location is not None:
+                location = urllib.parse.urljoin(url, location)
+                if not location.startswith(('http://', 'https://')):
+                    return RuntimeError('non-HTTP redirect')
+            try:
+                status = Status(
+                    code=response.status,
+                    location=location,
+                )
+            except ValueError as exc:
+                return exc
+            if status.redirect:
+                n_redirects += 1
+                if n_redirects > redirect_limit:
+                    raise RuntimeError('redirect limit exceeded')
+                if redirect_status and status.permanent_redirect:
+                    redirect_status = status
+                else:
+                    redirect_status = None
+                logger.debug('redirect {} -> {}'.format(url, location))
+                url = location
+                continue
+            if status.ok and isinstance(redirect_status, Status):
+                return redirect_status
+            return status
     return status
 
 _url_cache = {}
