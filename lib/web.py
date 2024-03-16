9f5df57862792bbca57a71e2886f86e621727b64
Jakub Wilk <jwilk@jwilk.net>
2024-03-16 15:54:29 +0100

    lib.web: use f-string.

--- lib/web.py
+++ lib/web.py
@@ -46,7 +46,8 @@ class Status():
         self.ok = self.code == http.HTTPStatus.OK
 
     def __str__(self):
-        return '{hs} {hs.phrase}'.format(hs=self.code)  # pylint: disable=missing-format-attribute
+        hs = self.code
+        return f'{hs} {hs.phrase}'  # pylint: disable=missing-format-attribute
 
 status_ok = Status(code=http.HTTPStatus.OK)
 assert status_ok.ok
